10a7871c4c35f6e4f5806e4c75f64411aa01af6d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-19 23:46:12 +0200

    Create menu-bar.js

---
+++ src/main/resources/static/js/menu-bar.js
@@ -0,0 +1,33 @@
+angular.module('menuBarApp', ['ngMaterial'])
+    .filter('keyboardShortcut', function($window) {
+        return function(str) {
+            if (!str) return;
+            var keys = str.split('-');
+            var isOSX = /Mac OS X/.test($window.navigator.userAgent);
+            var separator = (!isOSX || keys.length > 2) ? '+' : '';
+            var abbreviations = {
+                M: isOSX ? '⌘' : 'Ctrl',
+                A: isOSX ? 'Option' : 'Alt',
+                S: 'Shift'
+            };
+            return keys.map(function(key, index) {
+                var last = index === keys.length - 1;
+                return last ? key : abbreviations[key];
+            }).join(separator);
+        };
+    }).controller('menuBarCtrl', function MenuBarCtrl($mdDialog) {
+        this.settings = {
+            printLayout: true,
+            showRuler: true,
+            showSpellingSuggestions: true,
+            presentationMode: 'edit'
+        };
+        this.sampleAction = function(name, ev) {
+            $mdDialog.show($mdDialog.alert()
+                .title(name)
+                .textContent('You triggered the "' + name + '" action')
+                .ok('Great')
+                .targetEvent(ev)
+            );
+        };
+    });
