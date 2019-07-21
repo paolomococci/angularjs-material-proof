ab68a193259f847e9a4a4f7c4e15d061532f04ba
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-21 09:22:28 +0200

    Update autocomplete.js

--- src/main/resources/static/js/autocomplete.js
+++ src/main/resources/static/js/autocomplete.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('autocompleteProof', ['ngMaterial', 'ngMessages']).controller('AppCtrl', AppCtrl);
+    angular.module('autocompleteProof', ['ngMaterial', 'ngMessages']).controller('autocompleteCtrl', AppCtrl);
 
     function AppCtrl ($timeout, $q) {
         var self = this;
