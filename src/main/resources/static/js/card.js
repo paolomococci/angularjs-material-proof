d28706eb11c84650cc8c8008de4918571d6c21bd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-23 09:22:39 +0200

    Create card.js

---
+++ src/main/resources/static/js/card.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    angular.module('cardProof', ['ngMaterial'])
+        .controller('cardCtrl', function($scope) {
+            $scope.imagePath = 'img/card.svg';
+    });
+})();
