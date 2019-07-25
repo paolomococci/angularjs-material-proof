a50fde37de6c22c84f32e39d4e492755ada79fd8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-25 09:58:19 +0200

    Update card.js

--- src/main/resources/static/js/card.js
+++ src/main/resources/static/js/card.js
@@ -2,6 +2,15 @@
     'use strict';
     angular.module('cardProof', ['ngMaterial'])
         .controller('cardCtrl', function($scope) {
-            $scope.imagePath = 'img/card.svg';
-    });
+            $scope.voidImagePath = '/img/cards/void.svg';
+            $scope.starImagePath = '/img/cards/star.svg';
+            $scope.squareImagePath = '/img/cards/square.svg';
+            $scope.circleImagePath = '/img/cards/circle.svg';
+        })
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('some-lime').backgroundPalette('lime').dark();
+            $mdThemingProvider.theme('some-orange').backgroundPalette('orange').dark();
+            $mdThemingProvider.theme('some-purple').backgroundPalette('deep-purple').dark();
+            $mdThemingProvider.theme('some-red').backgroundPalette('red').dark();
+          });
 })();
