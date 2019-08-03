0880b7171f219cb95ba32067716fec6fb2ef5cd7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-03 11:58:51 +0200

    Update tooltip.js

--- src/main/resources/static/js/tooltip.js
+++ src/main/resources/static/js/tooltip.js
@@ -1,7 +1,14 @@
 (function () {
     'use strict';
     angular.module('tooltipProof', ['ngMaterial', 'ngMessages'])
-        .controller('tooltipCtrl', function() {
-
+        .controller('tooltipCtrl', function($scope) {
+            $scope.proof = {
+                showTooltip: false,
+                tipDirection: 'bottom'
+            };
+            $scope.proof.delayTooltip = undefined;
+            $scope.$watch('proof.delayTooltip', function(delay) {
+                $scope.proof.delayTooltip = parseInt(delay, 10) || 0;
+            });
     });
 })();
