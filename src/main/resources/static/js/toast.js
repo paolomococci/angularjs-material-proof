8aa1477eee254e18768411289da3aa4fa29c31a1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-11 16:49:49 +0200

    Update toast.js

--- src/main/resources/static/js/toast.js
+++ src/main/resources/static/js/toast.js
@@ -1,7 +1,26 @@
 (function () {
     'use strict';
-    angular.module('toastProof', ['ngMaterial', 'ngMessages'])
-        .controller('toastCtrl', function() {
-
-    });
+    angular.module('toastProof', ['ngMaterial'])
+        .controller('toastCtrl', toastCtrl);
+        function toastCtrl($mdToast, $log) {
+            var ctrl = this;
+            ctrl.showToast = function() {
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .textContent(
+                                'this is a sample of toast message like on android'
+                            )
+                            .action('OK')
+                            .hideDelay(3500))
+                            .then(function() {
+                                $log.log(
+                                    'last toast message vanished'
+                                );
+                            }).catch(function() {
+                                $log.log(
+                                    'error: toast failed or was forced to close early by request of another toast'
+                                );
+                            });
+            };
+        }
 })();
