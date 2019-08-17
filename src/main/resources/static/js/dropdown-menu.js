5de8dab5de98df9f3ec0b6182a2b931451ac87f4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-17 19:26:55 +0200

    Update dropdown-menu.js

--- src/main/resources/static/js/dropdown-menu.js
+++ src/main/resources/static/js/dropdown-menu.js
@@ -1,7 +1,45 @@
 (function () {
     'use strict';
     angular.module('dropdownMenuProof', ['ngMaterial'])
-        .controller('dropdownMenuCtrl', function() {
-
-    });
+        .config(function($mdIconProvider) {
+            $mdIconProvider
+                .icon("doc", '/img/icons/ic_book.svg', 24)
+                .icon("call-number", '/img/icons/ic_phone.svg', 24)
+                .icon("email", '/img/icons/ic_email.svg', 24)
+                .icon("chat", '/img/icons/ic_chat.svg', 24);
+        }).controller('dropdownMenuCtrl', function($mdDialog) {
+            var ctrl = this;
+            this.docClicked = function(index) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('you clicked on the document menu')
+                        .textContent('document item at index: ' + (++index))
+                        .ok('hi')
+                    );
+            };
+            this.phoneClicked = function(index) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('you clicked on the phone menu')
+                        .textContent('phone item at index: ' + (++index))
+                        .ok('hi')
+                    );
+            };
+            this.emailClicked = function(index) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('you clicked on the email menu')
+                        .textContent('email item at index: ' + (++index))
+                        .ok('hi')
+                    );
+            };
+            this.chatClicked = function(index) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('you clicked on the chat menu')
+                        .textContent('chat item at index: ' + (++index))
+                        .ok('hi')
+                    );
+            };
+        });
 })();
