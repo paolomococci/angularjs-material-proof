cec1a9d10512e1c6766a13bebbd25039d55498f6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-20 07:32:13 +0200

    Update sample-directives.js

--- src/main/resources/static/js/sample-directives.js
+++ src/main/resources/static/js/sample-directives.js
@@ -9,5 +9,26 @@
                 restrict: 'E',
                 template: "<h3>sample title element</h3>"
             }
+        })
+        .directive('pmArticle', function() {
+            return {
+                restrict: 'E',
+                template:
+                    "<article>" +
+                    "<h2>title of article</h2>" +
+                    "<section>" +
+                    "<h3>title of section one</h3>" +
+                    "<p>paragraph of section one</p>" +
+                    "</section>" +
+                    "<section>" +
+                    "<h3>title of section two</h3>" +
+                    "<p>paragraph of section two</p>" +
+                    "</section>" +
+                    "<section>" +
+                    "<h3>title of section three</h3>" +
+                    "<p>paragraph of section three</p>" +
+                    "</section>" +
+                    "</article>"
+            }
         });
 })();
