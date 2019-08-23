95a32021267fec29948a6e96ed3dc0e83f712558
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-23 07:54:25 +0200

    Update sample-directives.js

--- src/main/resources/static/js/sample-directives.js
+++ src/main/resources/static/js/sample-directives.js
@@ -13,45 +13,14 @@
         .directive('pmArticle', function() {
             return {
                 restrict: 'E',
-                template:
-                    "<article>" +
-                    "<h2>title of article</h2>" +
-                    "<section>" +
-                    "<h3>title of section one</h3>" +
-                    "<p>paragraph of section one</p>" +
-                    "</section>" +
-                    "<section>" +
-                    "<h3>title of section two</h3>" +
-                    "<p>paragraph of section two</p>" +
-                    "</section>" +
-                    "<section>" +
-                    "<h3>title of section three</h3>" +
-                    "<p>paragraph of section three</p>" +
-                    "</section>" +
-                    "</article>"
+                templateUrl: '/html/elements/pm-article.html'
             }
         })
         .directive('pmPerson', function() {
             return {
                 restrict: 'E',
                 $scope: { person: "=person" },
-                template:
-                    "<section>" +
-                    "<h3>section personal data</h3>" +
-                    "<p>name: <em>{{ person.name }}</em></p>" +
-                    "<p>surname: <em>{{ person.surname }}</em></p>" +
-                    "</section>" +
-                    "<section>" +
-                    "<form name='personForm'>" +
-                    "<fieldset>" +
-                    "<legend>your name, please</legend>" +
-                    "<label for='name'>name: </label>" +
-                    "<input type='text' id='name' ng-model='person.name'/>" +
-                    "<label for='surname'> surname: </label>" +
-                    "<input type='text' id='surname' ng-model='person.surname'/>" +
-                    "</fieldset>" +
-                    "</form>" +
-                    "</section>"
+                templateUrl: '/html/elements/pm-person.html'
             }
         })
         .directive('pmUser', function() {
