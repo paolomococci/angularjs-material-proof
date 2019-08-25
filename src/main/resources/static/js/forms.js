aaadce68b0ad2610e03d84eb03a2eb367274510e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-25 09:08:35 +0200

    Update forms.js

--- src/main/resources/static/js/forms.js
+++ src/main/resources/static/js/forms.js
@@ -1,7 +1,10 @@
 (function () {
     'use strict';
-    angular.module('formsProof', ['ngMaterial', 'ngMessages'])
-        .controller('formsCtrl', function() {
-
+    angular.module('formsProof', ['ngMaterial', 'ngSanitize'])
+        .controller('formsCtrl', function($scope) {
+            $scope.adminHelp = "<em class='help'>restricted area to admins</em>";
+            $scope.userHelp = "<em class='help'>area dedicated to common users</em>";
+            $scope.employeeHelp = "<em class='help'>employee shifts</em>";
+            $scope.jobHelp = "<em class='help'>planning of jobs</em>";
     });
 })();
