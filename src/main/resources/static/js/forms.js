74c72833f28dcabc1d40240f766447cbbd04cc6f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-08-27 06:50:27 +0200

    Update forms.js

--- src/main/resources/static/js/forms.js
+++ src/main/resources/static/js/forms.js
@@ -1,7 +1,11 @@
 (function () {
     'use strict';
     angular.module('formsProof', ['ngMaterial', 'ngSanitize'])
-        .config(function() {})
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('orange-dark')
+                .primaryPalette('orange')
+                .dark();
+        })
         .controller('formsCtrl', function($scope) {
             $scope.adminHelp = "<em class='help'>restricted area to admins</em>";
             $scope.userHelp = "<em class='help'>area dedicated to common users</em>";
