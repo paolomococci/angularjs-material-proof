0f9b5659f682d0290b34008f7e1582f0357ecdc2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-09-03 10:43:42 +0200

    Update todo-list.js

--- src/main/resources/static/js/todo-list.js
+++ src/main/resources/static/js/todo-list.js
@@ -1,6 +1,15 @@
 (function () {
     'use strict';
-    angular.module('todoListProof', ['ngMaterial'])
+    angular.module('todoListProof', ['ngMaterial', 'ngRoute'])
+        .config(function($routeProvider) {
+            $routeProvider
+                .when('/', {template: '<h2>home</h2>'})
+                .when('/login', {template: '<h2>login</h2>'})
+                .when('/photo', {template: '<h2>photo</h2>'})
+                .when('/share', {template: '<h2>share</h2>'})
+                .when('/chat', {template: '<h2>chat</h2>'})
+                .otherwise({redirectTo: '/'});
+        })
         .controller('todoListCtrl', function() {
 
     });
