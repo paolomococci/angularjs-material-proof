c9f4690790b6b6c5340f1234d3dfe744b9ee567d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-09-08 18:04:41 +0200

    Update todo-list.js

--- src/main/resources/static/js/todo-list.js
+++ src/main/resources/static/js/todo-list.js
@@ -13,38 +13,30 @@
                 .when('/share', {templateUrl: '/html/partials/todo-list-share.html'})
                 .otherwise({redirectTo: '/place'});
         })
-        .service('addToList', function() {
-            this.add = function(temp) {
-                this.list.push(temp);
+        .controller('todoListCtrl', function($scope) {
+            $scope.listOfPlaces =[];
+            $scope.listOfPeople = [];
+            $scope.listOfPhotos = [];
+            $scope.listOfShares = [];
+        })
+        .controller('placeCtrl', function($scope) {
+            $scope.addPlace = function() {
+                $scope.listOfPlaces.push($scope.place)
             }
         })
-        .controller('todoListCtrl', function($scope, addToList) {
-            /* place */
-            $scope.listOfPlaces = [
-                {
-                    name: 'Chiang Rai',
-                    country: 'Thailand',
-                    whatToDo: 'visit the white temple Wat Rong Khun',
-                    whenToDoIt: '12/5/2019 9:00AM',
-                    notes: "very beautiful"
-                },
-                {
-                    name: 'Chiang Rai',
-                    country: 'Thailand',
-                    whatToDo: 'visit Wat Phra Kaew',
-                    whenToDoIt: '12/5/2019 14:00PM',
-                    notes: "the ancient Wat Pa Yiah"
-                },
-                {
-                    name: 'Chiang Mai',
-                    country: 'Thailand',
-                    whatTodo: 'visit the Wat Phrathat Doi Suthep',
-                    whenToDoIt: '12/7/2019 10:00AM',
-                    notes: "perhaps I will listen to live folk music"
-                }
-            ];
-            $scope.addPlace = function() {
-                addToList.add();
+        .controller('peopleCtrl', function($scope) {
+            $scope.addPerson = function() {
+                $scope.listOfPeople.push($scope.name)
             }
-    });
+        })
+        .controller('photoCtrl', function($scope) {
+            $scope.addPhoto = function() {
+                $scope.listOfPhotos.push($scope.photo)
+            }
+        })
+        .controller('shareCtrl', function($scope) {
+            $scope.addShare = function() {
+                $scope.listOfShares.push($scope.share)
+            }
+        });
 })();
