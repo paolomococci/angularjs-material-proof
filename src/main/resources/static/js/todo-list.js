44cd55ad0e29d55b3cebfb8a9d54212e618a74b1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-09-10 20:42:32 +0200

    Update todo-list.js

--- src/main/resources/static/js/todo-list.js
+++ src/main/resources/static/js/todo-list.js
@@ -13,6 +13,12 @@
                 .when('/share', {templateUrl: '/html/partials/todo-list-share.html'})
                 .otherwise({redirectTo: '/place'});
         })
+        .service('peopleService', function() {
+                this.list = [];
+                this.addToList = function(temp) {
+                this.list.push(temp);
+            }
+        })
         .controller('todoListCtrl', function($scope) {
             $scope.listOfPlaces =[];
             $scope.listOfPeople = [];
@@ -24,19 +30,22 @@
 
             }
         })
-        .controller('peopleCtrl', function($scope) {
+        .controller('peopleCtrl', function($scope, peopleService) {
             $scope.addPerson = function() {
-                $scope.listOfPeople.push($scope.name)
+                peopleService.addToList($scope.name);
+                $scope.name = '';
+                $scope.personForm.name.$error = false;
             }
+            $scope.listOfPeople = peopleService.list;
         })
         .controller('photoCtrl', function($scope) {
             $scope.addPhoto = function() {
-                
+
             }
         })
         .controller('shareCtrl', function($scope) {
             $scope.addShare = function() {
-                
+
             }
         });
 })();
